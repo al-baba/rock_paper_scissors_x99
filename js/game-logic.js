a2b736bba4167ac5ac4c397ac18897caa235378d
al-baba <kentarochesney@gmail.com>
2022-06-26 17:44:20 +0100

    first commit

---
+++ js/game-logic.js
@@ -0,0 +1,33 @@
+// All code should be written in this file.
+
+let playerOneMoveOneType = undefined
+let playerOneMoveOneValue = undefined
+
+let playerOneMoveTwoType = undefined
+let playerOneMoveTwoValue = undefined
+
+let playerOneMoveThreeType = undefined
+let playerOneMoveThreeValue = undefined
+
+
+let playerTwoMoveOneType = undefined
+let playerTwoMoveOneValue = undefined
+
+let playerTwoMoveTwoType = undefined
+let playerTwoMoveTwoValue = undefined
+
+let playerTwoMoveThreeType = undefined
+let playerTwoMoveThreeValue = undefined
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+
+};
+
+const getGameWinner = (playerOneMoveType, playerTwoMoveType) => {
+    return winner
+};
+
+// const setComputerMoves = () => {
+
+// }
+    
